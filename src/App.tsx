463bfd143011d0cd951a1ea569839e20fc208a53
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-12-14 11:13:01 +0300

    react+typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
-function App() {
+interface Props {
+  name: string;
+  url: string;
+};
+
+
+const App: React.FC<Props> = ({ name, url }) => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <h1>Hello, {name}!</h1>
+    <img src={url} alt='chat-gpt' />
+  </div>
+  )
+};
 
 export default App;
